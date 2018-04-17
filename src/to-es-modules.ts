039fe5aa5dda931ceac40b924c4526cb825a319e
epicallan <epicallan.al@gmail.com>
2018-04-17 11:28:52 +0300

    minor changes

--- src/to-es-modules.ts
+++ src/to-es-modules.ts
@@ -6,15 +6,12 @@ import {resolve} from 'path';
 import {replace, match} from 'ramda';
 import * as lebab from 'lebab';
 import * as yargs from 'yargs';
-import {readdir, readFile, writeFile, unlink } from 'fs-extra';
+import {readdir, readFile, writeFile} from 'fs-extra';
 
 const currentDir = process.cwd();
 
 const codeTransforms =
-    [   'let', 'commonjs', 'template',
-        'destruct-param', 'includes', 'obj-shorthand',
-        'no-strict', 'multi-var'
-    ];
+    [ 'let', 'commonjs' ];
 
 const allJsFilesInDir = async (dir: string): Promise<string[]> =>
     (await readdir(dir))
@@ -27,7 +24,7 @@ export const extraTransforms = (content: string) => {
     return replace(/exports\./gm, '', (content));
 };
 
-export const convertToTs = async (relativeDir?: string) => {
+export const convertToEs = async (relativeDir?: string) => {
     const dir = relativeDir ? resolve(currentDir, relativeDir) : currentDir;
     console.info('converting files in ', dir);
     const allFiles = await allJsFilesInDir(dir);
@@ -37,28 +34,28 @@ export const convertToTs = async (relativeDir?: string) => {
             const content = await readFile(jsFullPath, 'utf8');
             const {code, warnings} = lebab.transform(content, codeTransforms);
             console.log('\n ** warnings ***', warnings, '*** \n');
-            const tsPath = newTsPath(jsFullPath);
-            await writeFile(tsPath, extraTransforms(code));
+            // const tsPath = newTsPath(jsFullPath);
+            await writeFile(jsFullPath, extraTransforms(code));
         });
 };
 
-export const cleanup =  async (relativeDir: string = currentDir) => {
-    const dir = relativeDir ? resolve(currentDir, relativeDir) : currentDir;
-    console.info('deleting files in ', dir);
-    const allFiles = await allJsFilesInDir(dir);
-    allFiles
-        .forEach(async (fileName) => {
-            const jsFullPath = resolve(dir, fileName);
-            unlink(jsFullPath);
-        });
-};
+// export const cleanup =  async (relativeDir: string = currentDir) => {
+//     const dir = relativeDir ? resolve(currentDir, relativeDir) : currentDir;
+//     console.info('deleting files in ', dir);
+//     const allFiles = await allJsFilesInDir(dir);
+//     allFiles
+//         .forEach(async (fileName) => {
+//             const jsFullPath = resolve(dir, fileName);
+//             unlink(jsFullPath);
+//         });
+// };
 
 const main = async () => {
     try {
-        if (yargs.argv.d && yargs.argv.dir) return cleanup(yargs.argv.dir);
-        if (yargs.argv.d) return cleanup();
-        if (yargs.argv.dir) return convertToTs(yargs.argv.dir);
-        return convertToTs();
+        // if (yargs.argv.d && yargs.argv.dir) return cleanup(yargs.argv.dir);
+        // if (yargs.argv.d) return cleanup();
+        if (yargs.argv.dir) return convertToEs(yargs.argv.dir);
+        return convertToEs();
      } catch (err) {
          console.error(err);
      }
